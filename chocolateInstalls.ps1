b04800bc5fc989640aad4bcdb6e37d47b24ea873
Elias Santistevan <elias.santistevan@sparkfun.com>
2020-10-25 06:43:04 -0600

    adds cloning script, and .env from nvidia snatcher

--- chocolateInstalls.ps1
+++ chocolateInstalls.ps1
@@ -8,31 +8,37 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 #Various Apps
 choco install spotify -y 
 choco install brave -y 
+choco install eagle -y 
+choco install telegram -y 
 
 # Git and Terminals
 choco install github-desktop -y 
 choco install git -y 
 choco install curl -y 
-choco install cygwin -y 
-choco install teraterm -y 
 
 # Games
 choco install steam -y 
 choco install epicgameslauncher -y 
+#battle.net
+choco install geforce-experience -y 
+choco install hwinfo -y
+choco install discord -y
+choco install steelseries-engine -y
+choco install razer-synapse-2 -y
 
 # VIM 
 choco install vim -y 
 choco install cppcheck -y 
 choco install universal-ctags -y 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Programming Languages
-choco install nodejs-lts -y 
+choco install nodejs -y 
 choco install python -y 
 choco install arduino -y 
 
 choco install boostnote -y
+choco install vnc-viewer -y 
 choco install google-backup-and-sync -y  
 choco install foxitreader -y
-# Salae logic for logica analyzer
 # Any games from Steam
+
---
+++ repostiories.ps1
@@ -0,0 +1,14 @@
+#Place these repositories into C:/users/user/documents/Github
+# Personal
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/santised/wonder_dotfiles.git
+git clone https://github.com/santised/Eagle_Settings.git
+git clone https://github.com/santised/choco_installs.git
+https://github.com/jef/nvidia-snatcher.git
+
+# Work
+git clone https://github.com/santised/Eagle_Settings.git
+git clone https://github.com/sparkfun/SparkFun-Eagle-Libraries.git
+git clone https://github.com/sparkfun/SparkFun-Eagle-Settings.git
+git clone https://github.com/sparkfun/Raspberry_pi_adapter.git
+
